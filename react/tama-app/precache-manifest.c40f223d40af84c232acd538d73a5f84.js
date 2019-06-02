dafc2010af75ebf1f500c8ca7f440858361744b0
Anhtu Ta <anhtu51195@gmail.com>
2019-06-02 16:36:52 +0700

    add tama-app

---
+++ react/tama-app/precache-manifest.c40f223d40af84c232acd538d73a5f84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354be3ce77e8123c58fb7f6f4d492c82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "467af6dbcf4d77fa27b3",
+    "url": "./static/css/main.f0e3738d.chunk.css"
+  },
+  {
+    "revision": "7c48a29b5e1e6e37689e",
+    "url": "./static/js/2.2cf93f6c.chunk.js"
+  },
+  {
+    "revision": "467af6dbcf4d77fa27b3",
+    "url": "./static/js/main.fc1d37c4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
